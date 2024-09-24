46722a05ea97a4bdab4abb380eb6ee085a6d8afe
eiurrutia <eiurrutia@uc.cl>
2024-09-23 21:03:00 -0300

    feat: item color autocomplete api

---
+++ pages/api/trade-in/autocomplete.js
@@ -0,0 +1,22 @@
+import { executeQuery } from '@/app/lib/snowflakeClient';
+
+export default async function handler(req, res) {
+  const query = `
+    SELECT DISTINCT ITEM_COLOR, IMAGE_SRC
+    FROM PATAGONIA.CORE_TEST.SHOPIFY_PRODUCTS_IMAGE_EMBEDDINGS
+    WHERE ITEM_COLOR NOT LIKE 'W%'
+  `;
+  
+  try {
+    const results = await executeQuery(query);
+    const itemColorsWithImages = results.map(row => ({
+      itemColor: row.ITEM_COLOR,
+      imageSrc: row.IMAGE_SRC,
+    }));
+
+    res.status(200).json({ itemColors: itemColorsWithImages });
+  } catch (error) {
+    console.error('Error fetching ITEM_COLOR and IMAGE_SRC:', error);
+    res.status(500).json({ error: 'Error fetching ITEM_COLOR and IMAGE_SRC' });
+  }
+}
