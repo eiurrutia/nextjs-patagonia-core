823a171cc2f62025eba5bf0af6f0a325bb38cd2f
eiurrutia <eiurrutia@uc.cl>
2024-10-20 18:21:01 -0300

    feat: home designs

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,20 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <PatagoniaLogo />
-        {/* <AcmeLogo /> */}
+      <div className="relative flex items-center rounded-lg bg-blue-500 md:h-64">
+        <div className="absolute left-4 pl-8 flex items-center">
+          <div className="h-full max-h-64 md:max-h-64">
+            <PatagoniaLogo />
+          </div>
+        </div>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-4 md:w-2/5 md:px-20">
         <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Bienvenido a Patagonia Core!</strong> La solución propia de{' '}
+            Patagonia Chile para centralizar la información de sus distintas plataformas.
           </p>
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <Link
@@ -30,10 +30,9 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+        <div className="flex items-center justify-center">
           <Image
-            src="/hero-desktop.png"
+            src="/fondo.png"
             width={1000}
             height={760}
             className="hidden md:block"
