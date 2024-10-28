4f2352d5c3389f0f9930336b6fc483f8c80426d6
eiurrutia <eiurrutia@uc.cl>
2024-10-27 21:27:35 -0300

    fix: seervice worker registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,31 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch((registrationError) => {
+          console.error('Service Worker registration failed: ', registrationError);
+        });
+    }
+  }, []);
+
   return (
     <html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/patagonia-core-icon.png" />
         <meta name="theme-color" content="#0070f3" />
         <title>Patagonia Core</title>
       </Head>
