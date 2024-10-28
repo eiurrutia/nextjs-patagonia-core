1a4e734d0042c5c0d38de61e55b0a4817fcd0abe
eiurrutia <eiurrutia@uc.cl>
2024-10-27 21:36:10 -0300

    fix: service worker code

--- public/sw.js
+++ public/sw.js
@@ -1,43 +1,23 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// If the loader is already loaded, just stop.
 if (!self.define) {
   let registry = {};
-
-  // Used for `eval` and `importScripts` where we can't get script URL by other means.
-  // In both cases, it's safe to use a global var because those functions are synchronous.
   let nextDefineUri;
 
   const singleRequire = (uri, parentUri) => {
     uri = new URL(uri + ".js", parentUri).href;
     return registry[uri] || (
-      
-        new Promise(resolve => {
-          if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = uri;
-            script.onload = resolve;
-            document.head.appendChild(script);
-          } else {
-            nextDefineUri = uri;
-            importScripts(uri);
-            resolve();
-          }
-        })
-      
-      .then(() => {
-        let promise = registry[uri];
+      new Promise(resolve => {
+        if ("document" in self) {
+          const script = document.createElement("script");
+          script.src = uri;
+          script.onload = resolve;
+          document.head.appendChild(script);
+        } else {
+          nextDefineUri = uri;
+          importScripts(uri);
+          resolve();
+        }
+      }).then(() => {
+        const promise = registry[uri];
         if (!promise) {
           throw new Error(`Module ${uri} didn’t register its module`);
         }
@@ -48,39 +28,32 @@ if (!self.define) {
 
   self.define = (depsNames, factory) => {
     const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
-    if (registry[uri]) {
-      // Module is already loading or loaded.
-      return;
-    }
+    if (registry[uri]) return;
+
     let exports = {};
     const require = depUri => singleRequire(depUri, uri);
-    const specialDeps = {
-      module: { uri },
-      exports,
-      require
-    };
-    registry[uri] = Promise.all(depsNames.map(
-      depName => specialDeps[depName] || require(depName)
-    )).then(deps => {
-      factory(...deps);
-      return exports;
-    });
+    const specialDeps = { module: { uri }, exports, require };
+
+    registry[uri] = Promise.all(depsNames.map(dep => specialDeps[dep] || require(dep)))
+      .then(deps => {
+        factory(...deps);
+        return exports;
+      });
   };
 }
-define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
+
+define(['./workbox-e43f5367'], function (workbox) {
+  'use strict';
 
   importScripts();
   self.skipWaiting();
   workbox.clientsClaim();
+
+  // Cache home page and fallback to network.
   workbox.registerRoute("/", new workbox.NetworkFirst({
-    "cacheName": "start-url",
+    cacheName: "start-url",
     plugins: [{
-      cacheWillUpdate: async ({
-        request,
-        response,
-        event,
-        state
-      }) => {
+      cacheWillUpdate: async ({ response }) => {
         if (response && response.type === 'opaqueredirect') {
           return new Response(response.body, {
             status: 200,
@@ -92,10 +65,26 @@ define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
       }
     }]
   }), 'GET');
-  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
-    "cacheName": "dev",
-    plugins: []
-  }), 'GET');
 
-}));
-//# sourceMappingURL=sw.js.map
+  // Avoid caching unnecessary or problematic files.
+  workbox.registerRoute(
+    ({ url }) => !url.pathname.includes('app-build-manifest.json'),
+    new workbox.NetworkOnly({ cacheName: 'dev' }),
+    'GET'
+  );
+
+  // Optional: Cache other static assets (e.g., images or CSS files).
+  workbox.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 días
+        }),
+      ],
+    }),
+    'GET'
+  );
+});
