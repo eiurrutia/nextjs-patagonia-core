f110d78192d6320db931b59a056f114218c346bd
eiurrutia <eiurrutia@uc.cl>
2024-10-27 21:06:55 -0300

    fix: layout manifest definition

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,8 @@
-// import '@/app/ui/global.css';
-// import { inter } from '@/app/ui/fonts';
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={`${inter.className} antialiased`}>{children}</body>
-//     </html>
-//   );
-// }
-
-
 'use client';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -26,6 +11,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#0070f3" />
+        <title>Patagonia Core</title>
+      </Head>
       <body className={`${inter.className} antialiased`}>
         <SessionProvider>{children}</SessionProvider>
       </body>
