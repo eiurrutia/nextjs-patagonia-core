b1748fba55e76ad156077c66d084411c02239fc5
eiurrutia <eiurrutia@uc.cl>
2024-11-22 15:02:15 -0300

    feat: modify size limit in request to save segmentation history

--- pages/api/stock-planning/save-segmentation-history.ts
+++ pages/api/stock-planning/save-segmentation-history.ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { saveSegmentationHistory } from '@/app/lib/stock-planning/data';
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
