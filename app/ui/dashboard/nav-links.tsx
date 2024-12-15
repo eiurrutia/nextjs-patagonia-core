6e10ed99a7a776a38ba39c8f1acd5d6707601dea
eiurrutia <eiurrutia@uc.cl>
2024-12-15 20:06:23 -0300

    feat: roles filters in new items and subitems

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import clsx from 'clsx';
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import CollapsibleNavItem from './collapsible-nav-item';
@@ -16,40 +15,44 @@ import {
   Cog8ToothIcon,
   CalculatorIcon,
   RectangleGroupIcon,
-  PresentationChartBarIcon
+  PresentationChartBarIcon,
 } from '@heroicons/react/24/outline';
 
 const navItems = [
   {
     name: 'CCSS',
     icon: UserGroupIcon,
+    roles: ['admin', 'user'],
     items: [
-      { name: 'Dashboard', href: null, icon: PresentationChartBarIcon },
-      { name: 'Orders', href: '/dashboard/orders', icon: DocumentDuplicateIcon },
-      { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-      { name: 'Incidences', href: '/dashboard/incidences', icon: BellAlertIcon },
+      { name: 'Dashboard', href: null, icon: PresentationChartBarIcon, roles: ['admin', 'user'] },
+      { name: 'Orders', href: '/dashboard/orders', icon: DocumentDuplicateIcon, roles: ['admin', 'user'] },
+      { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon, roles: ['admin', 'user'] },
+      { name: 'Incidences', href: '/dashboard/incidences', icon: BellAlertIcon, roles: ['admin', 'user'] },
     ],
   },
   {
     name: 'Stock',
     icon: RectangleGroupIcon,
+    roles: ['admin'],
     items: [
-      { name: 'Stock Planning', href: '/dashboard/stock-planning', icon: CalculatorIcon },
+      { name: 'Stock Planning', href: '/dashboard/stock-planning', icon: CalculatorIcon, roles: ['admin'] },
     ],
   },
   {
     name: 'Trade In',
     icon: ArrowPathIcon,
+    roles: ['admin'],
     items: [
-      { name: 'Registros', href: '/trade-in', icon: DocumentDuplicateIcon },
+      { name: 'Registros', href: '/trade-in', icon: DocumentDuplicateIcon, roles: ['admin'] },
     ],
   },
   {
     name: 'Settings',
     icon: Cog8ToothIcon,
+    roles: ['admin', 'user'],
     items: [
-      { name: 'Users', href: '/dashboard/users', icon: UserIcon },
-      { name: 'Parámetros', href: '/dashboard/configs', icon: AdjustmentsVerticalIcon },
+      { name: 'Users', href: '/dashboard/users', icon: UserIcon, roles: ['admin'] },
+      { name: 'Parámetros', href: '/dashboard/configs', icon: AdjustmentsVerticalIcon, roles: ['admin', 'user'] },
     ],
   },
 ];
@@ -74,21 +77,22 @@ export default function NavLinks() {
       <Link
         href="/dashboard"
         className='flex items-center gap-2 px-3 py-4 rounded-md font-medium text-gray-700 bg-gray-50 hover:bg-sky-100 hover:text-blue-600'
-        
       >
         <HomeIcon className="w-5 h-5 text-gray-500" />
         <span>Home</span>
       </Link>
 
       {/* Collapsible Items */}
-      {navItems.map((section) => (
-        <CollapsibleNavItem
-          key={section.name}
-          name={section.name}
-          icon={section.icon}
-          items={section.items}
-        />
-      ))}
+      {navItems
+        .filter((section) => section.roles.includes(userRole))
+        .map((section) => (
+          <CollapsibleNavItem
+            key={section.name}
+            name={section.name}
+            icon={section.icon}
+            items={section.items.filter((item) => item.roles.includes(userRole))}
+          />
+        ))}
     </div>
   );
 }
