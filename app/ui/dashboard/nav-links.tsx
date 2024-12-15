370f815cedf6934685f501a0dbd405dff9696832
eiurrutia <eiurrutia@uc.cl>
2024-12-15 19:56:55 -0300

    feat: collapsable items in side bar

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,74 +1,62 @@
 'use client';
+import React from 'react';
+import Link from 'next/link';
+import clsx from 'clsx';
+import { useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
+import CollapsibleNavItem from './collapsible-nav-item';
 import {
-  UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  UserGroupIcon,
   BellAlertIcon,
   ArrowPathIcon,
   AdjustmentsVerticalIcon,
   UserIcon,
-  Cog8ToothIcon
+  Cog8ToothIcon,
+  CalculatorIcon,
+  RectangleGroupIcon,
+  PresentationChartBarIcon
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
-const links = [
+const navItems = [
   {
-    name: 'Home',
-    href: '/dashboard',
-    icon: HomeIcon,
-    roles: ['admin', 'user'],
-  },
-  {
-    name: 'Orders',
-    href: '/dashboard/orders',
-    icon: DocumentDuplicateIcon,
-    roles: ['admin', 'user'],
-  },
-  { 
-    name: 'Customers',
-    href: '/dashboard/customers',
+    name: 'CCSS',
     icon: UserGroupIcon,
-    roles: ['admin', 'user'],
+    items: [
+      { name: 'Dashboard', href: null, icon: PresentationChartBarIcon },
+      { name: 'Orders', href: '/dashboard/orders', icon: DocumentDuplicateIcon },
+      { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+      { name: 'Incidences', href: '/dashboard/incidences', icon: BellAlertIcon },
+    ],
   },
-  { 
-    name: 'Incidences',
-    href: '/dashboard/incidences',
-    icon: BellAlertIcon,
-    roles: ['admin', 'user'],
+  {
+    name: 'Stock',
+    icon: RectangleGroupIcon,
+    items: [
+      { name: 'Stock Planning', href: '/dashboard/stock-planning', icon: CalculatorIcon },
+    ],
   },
-  { 
-    name: 'Trade-In',
-    href: '/trade-in',
+  {
+    name: 'Trade In',
     icon: ArrowPathIcon,
-    roles: ['admin'],
+    items: [
+      { name: 'Registros', href: '/trade-in', icon: DocumentDuplicateIcon },
+    ],
   },
-  { 
-    name: 'Stock Planning',
-    href: '/dashboard/stock-planning',
-    icon: AdjustmentsVerticalIcon,
-    roles: ['admin'],
-  },
-  { 
-    name: 'Users',
-    href: '/dashboard/users',
-    icon: UserIcon,
-    roles: ['admin'],
-  },
-  { 
+  {
     name: 'Settings',
-    href: '/dashboard/configs',
     icon: Cog8ToothIcon,
-    roles: ['admin', 'user'],
+    items: [
+      { name: 'Users', href: '/dashboard/users', icon: UserIcon },
+      { name: 'Parámetros', href: '/dashboard/configs', icon: AdjustmentsVerticalIcon },
+    ],
   },
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
   const { data: session, status } = useSession();
+  const pathname = usePathname();
 
   if (status === 'loading') {
     return <p>Loading...</p>;
@@ -81,27 +69,26 @@ export default function NavLinks() {
   const userRole = session.user.role;
 
   return (
-    <>
-      {links
-        .filter((link) => link.roles.includes(userRole as string))
-        .map((link) => {
-          const LinkIcon = link.icon;
-          return (
-            <Link
-              key={link.name}
-              href={link.href}
-              className={clsx(
-                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                {
-                  'bg-sky-100 text-blue-600': pathname === link.href,
-                },
-              )}
-            >
-              <LinkIcon className="w-6" />
-              <p className="hidden md:block">{link.name}</p>
-            </Link>
-          );
-        })}
-    </>
+    <div>
+      {/* Home */}
+      <Link
+        href="/dashboard"
+        className='flex items-center gap-2 px-3 py-4 rounded-md font-medium text-gray-700 bg-gray-50 hover:bg-sky-100 hover:text-blue-600'
+        
+      >
+        <HomeIcon className="w-5 h-5 text-gray-500" />
+        <span>Home</span>
+      </Link>
+
+      {/* Collapsible Items */}
+      {navItems.map((section) => (
+        <CollapsibleNavItem
+          key={section.name}
+          name={section.name}
+          icon={section.icon}
+          items={section.items}
+        />
+      ))}
+    </div>
   );
 }
