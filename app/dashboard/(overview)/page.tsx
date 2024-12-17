cd26a323c091d688a8e9d0ba1be3cb27437d121e
eiurrutia <eiurrutia@uc.cl>
2024-12-17 16:50:47 -0300

    feat: home buttons

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -6,6 +7,7 @@ import InOmsNoErpDifferenceCard from '@/app/ui/dashboard/in-oms-no-erp-differenc
 import InShopifyNoOmsDifferenceCard from '@/app/ui/dashboard/in-shopify-no-oms-difference-card';
 import QuantityDiscrepancyCard from '@/app/ui/dashboard/quantity-discrepanciy-card';
 import OpenedIncidencesCard from '@/app/ui/dashboard/opened-incidences-card';
+import { UserGroupIcon, RectangleGroupIcon, ArrowPathIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { CardSkeleton } from '@/app/ui/skeletons';
@@ -15,11 +17,53 @@ import { format, subMonths } from 'date-fns';
 export default function Page() {
     const [startDate, setStartDate] = useState(subMonths(new Date(), 1));
     const [endDate, setEndDate] = useState(new Date());
+    const router = useRouter();
+
+    const sections = [
+        {
+            title: 'CCSS',
+            description: 'Accede a la gestión de CCSS.',
+            route: '/ccss',
+            color: 'bg-blue-500',
+            icon: UserGroupIcon,
+        },
+        {
+            title: 'STOCK PLANNING',
+            description: 'Planifica y gestiona el stock.',
+            route: '/stock-planning',
+            color: 'bg-green-500',
+            icon: RectangleGroupIcon,
+        },
+        {
+            title: 'TRADE-IN',
+            description: 'Gestiona las operaciones de trade-in.',
+            route: '/trade-in',
+            color: 'bg-yellow-500',
+            icon: ArrowPathIcon,
+        },
+    ];
+
     return (
         <main>
-        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-            Patagonia Core
-        </h1>
+        
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className={`${lusitana.className} mb-8 text-8xl font-bold  md:text-6xl`}>
+                Patagonia Core
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-5xl">
+                {sections.map((section) => (
+                    <div
+                        key={section.title}
+                        onClick={() => router.push(section.route)}
+                        className={`flex flex-col items-center justify-center h-48 rounded-lg shadow-lg cursor-pointer ${section.color} text-white transform hover:scale-105 transition-transform`}
+                    >
+                        <section.icon className="h-12 w-12 mb-4 text-white" />
+                        <h2 className="text-2xl font-bold">{section.title}</h2>
+                        <p className="mt-2 text-center px-4">{section.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
         <div className="flex flex-wrap gap-4 mb-4">
             <div className="w-full sm:w-auto">
                 <label htmlFor="start-date" className="block text-sm font-medium text-gray-700">Fecha Inicio</label>
